516be43224f4bf627a0ce5679b21c3699d6a4f9b
Philo Maqar <philo.maqar@gmail.com>
2025-12-24 17:28:07 -0500

    first commit

---
+++ server/src/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use('/', (req, res) => {
+
+    res.send('Hello, World!');  
+
+});
+app.listen(PORT, () => {
+
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+
+
